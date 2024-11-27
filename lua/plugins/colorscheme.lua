61f6489b29baced062d6210a4c781837ee362853
kei0822kei <mizokami.keiyu@gmail.com>
2024-11-27 23:03:21 +0900

    Fix: Fix colorscheme setting bug.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,16 @@
 return {
   "folke/tokyonight.nvim",
   lazy = false,
-  style = "storm",
   opts = {
+    style = "storm",
     on_highlights = function(hl)
       hl.comment = { bg = "#4a691c", fg = "#000000" }
       hl.perlComment = { bg = "#4a691c", fg = "#000000" }
       hl.Comment = { bg = "#4a691c", fg = "#000000" }
     end,
   },
+  -- "LazyVim/LazyVim",
+  -- opts = {
+  --   colorscheme = "tokyonight",
+  -- },
 }
