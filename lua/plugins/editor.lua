262ba477b519cc6eb0682cc073fe29da12ec907f
kei0822kei <mizokami.keiyu@gmail.com>
2024-11-27 22:07:15 +0900

    Add: Add lualine and flash custom mappings.

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,26 +1,45 @@
 return {
-  "kevinhwang91/nvim-hlslens",
-  lazy = true,
-  opts = {},
-  config = function()
-    local kopts = { noremap = true, silent = true }
-    vim.api.nvim_set_keymap(
-      "n",
-      "n",
-      [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]],
-      kopts
-    )
-    vim.api.nvim_set_keymap(
-      "n",
-      "N",
-      [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]],
-      kopts
-    )
-    vim.api.nvim_set_keymap("n", "*", [[*<Cmd>lua require('hlslens').start()<CR>]], kopts)
-    vim.api.nvim_set_keymap("n", "#", [[#<Cmd>lua require('hlslens').start()<CR>]], kopts)
-    vim.api.nvim_set_keymap("n", "g*", [[g*<Cmd>lua require('hlslens').start()<CR>]], kopts)
-    vim.api.nvim_set_keymap("n", "g#", [[g#<Cmd>lua require('hlslens').start()<CR>]], kopts)
+  {
+    "kevinhwang91/nvim-hlslens",
+    lazy = true,
+    opts = {},
+    config = function()
+      local kopts = { noremap = true, silent = true }
+      vim.api.nvim_set_keymap(
+        "n",
+        "n",
+        [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]],
+        kopts
+      )
+      vim.api.nvim_set_keymap(
+        "n",
+        "N",
+        [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]],
+        kopts
+      )
+      vim.api.nvim_set_keymap("n", "*", [[*<Cmd>lua require('hlslens').start()<CR>]], kopts)
+      vim.api.nvim_set_keymap("n", "#", [[#<Cmd>lua require('hlslens').start()<CR>]], kopts)
+      vim.api.nvim_set_keymap("n", "g*", [[g*<Cmd>lua require('hlslens').start()<CR>]], kopts)
+      vim.api.nvim_set_keymap("n", "g#", [[g#<Cmd>lua require('hlslens').start()<CR>]], kopts)
 
-    vim.api.nvim_set_keymap("n", "<Leader>l", "<Cmd>noh<CR>", kopts)
-  end,
+      vim.api.nvim_set_keymap("n", "<Leader>l", "<Cmd>noh<CR>", kopts)
+    end,
+  },
+  {
+    "folke/flash.nvim",
+    keys = {
+      {
+        "m",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash").jump()
+        end,
+        desc = "Flash",
+      },
+      { "s", false },
+      { "S", false },
+      { "r", false },
+      { "R", false },
+    },
+  },
 }
