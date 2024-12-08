52769f84380ccf55e7bdd21de9abc285158f7d82
kei0822kei <mizokami.keiyu@gmail.com>
2024-12-08 23:48:40 +0900

    Add: Default color to kanagawa.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,14 +3,22 @@ return {
   lazy = false,
   opts = {
     style = "storm",
-    on_highlights = function(hl)
-      hl.comment = { bg = "#4a691c", fg = "#000000" }
-      hl.perlComment = { bg = "#4a691c", fg = "#000000" }
-      hl.Comment = { bg = "#4a691c", fg = "#000000" }
-    end,
+    -- on_highlights = function(hl)
+    --   hl.comment = { bg = "#4a691c", fg = "#000000" }
+    --   hl.perlComment = { bg = "#4a691c", fg = "#000000" }
+    --   hl.Comment = { bg = "#4a691c", fg = "#000000" }
+    -- end,
   },
-  -- "LazyVim/LazyVim",
-  -- opts = {
-  --   colorscheme = "tokyonight",
-  -- },
+  { "itchyny/landscape.vim" },
+  { "shaunsingh/nord.nvim" },
+  { "tiagovla/tokyodark.nvim" },
+  { "navarasu/onedark.nvim" },
+  { "Mofiqul/dracula.nvim" },
+  { "rebelot/kanagawa.nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa",
+    },
+  },
 }
