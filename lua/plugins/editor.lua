ad37fbe108286e261a8e5e7dbd28cccd594041b3
kei0822kei <mizokami.keiyu@gmail.com>
2024-12-14 11:44:10 +0900

    Add: Customize flash color.

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,3 +1,13 @@
+local function setupCustomHighlightGroup()
+  vim.api.nvim_command("hi clear FlashMatch")
+  vim.api.nvim_command("hi clear FlashCurrent")
+  vim.api.nvim_command("hi clear FlashLabel")
+
+  vim.api.nvim_command("hi FlashMatch guibg=#4A47A3 guifg=#B8B5FF") -- Emerald background
+  vim.api.nvim_command("hi FlashCurrent guibg=#456268 guifg=#D0E8F2")
+  vim.api.nvim_command("hi FlashLabel guibg=#A25772 guifg=#EEF5FF")
+end
+
 return {
   {
     "kevinhwang91/nvim-hlslens",
@@ -41,5 +51,23 @@ return {
       { "r", false },
       { "R", false },
     },
+    opts = {
+      rainbow = {
+        enabled = true,
+        shade = 5,
+      },
+      highlight = {
+        backdrop = true,
+        groups = {
+          match = "FlashMatch",
+          current = "FlashCurrent",
+          backdrop = "FlashBackdrop",
+          label = "FlashLabel",
+        },
+      },
+    },
+    config = function()
+      setupCustomHighlightGroup()
+    end,
   },
 }
