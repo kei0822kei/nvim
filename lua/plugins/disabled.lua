84bf3bfa6652e071df32d942a00040d1b37a34bf
kei0822kei <mizokami.keiyu@gmail.com>
2025-02-01 12:39:44 +0900

    Add: Set conceallevel=0 and disabled render-markdown.

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,4 +1,5 @@
 return {
   { "nvim-treesitter/nvim-treesitter", enabled = false },
   { "nvim-treesitter/nvim-treesitter-textobjects", enabled = false },
+  { "MeanderingProgrammer/render-markdown.nvim", enabled = false },
 }
