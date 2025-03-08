623d4dbeb7ab8ba092aca4c5b31efbd66e9f3cee
kei0822kei <mizokami.keiyu@gmail.com>
2025-03-08 16:58:57 +0900

    Add: Update settings.

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,5 +1,6 @@
 return {
-  { "nvim-treesitter/nvim-treesitter", enabled = false },
-  { "nvim-treesitter/nvim-treesitter-textobjects", enabled = false },
-  { "MeanderingProgrammer/render-markdown.nvim", enabled = false },
+  --{ "nvim-treesitter/nvim-treesitter", enabled = false },
+  --{ "nvim-treesitter/nvim-treesitter-textobjects", enabled = false },
+  { "sheerun/vim-polyglot", enabled = false },
+  --{ "MeanderingProgrammer/render-markdown.nvim", enabled = false },
 }
